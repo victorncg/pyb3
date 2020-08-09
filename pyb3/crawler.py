b22a73006786bc7791160ee05b5f40e2cae2b57e
Fábio <fabiomt92@hotmail.com>
2020-08-09 20:37:20 -0300

    v1

--- pyb3/crawler.py
+++ pyb3/crawler.py
@@ -1,6 +1,7 @@
 
 
 import requests
+import pandas as pd
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
