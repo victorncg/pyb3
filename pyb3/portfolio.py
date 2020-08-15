2111145d0fb2c0b52800e004a70fb2faa1be2faa
Fábio <fabiomt92@hotmail.com>
2020-08-15 20:18:50 -0300

    v3

--- pyb3/portfolio.py
+++ pyb3/portfolio.py
@@ -1,6 +1,6 @@
 
 import pandas as pd
-from crawler import acoes
+from pyb3 import crawler.acoes
 from scipy.stats import norm
 
 
@@ -10,7 +10,7 @@ class Carteira:
         ativos = ativos if type(ativos)==list else [ativos]
         periodo = periodo if type(periodo)==list else [periodo]
         volumes = volumes if type(volumes)==list else [volumes]
-        series = acoes.UolSeries().get(ativos, intraday, periodo, dataini)
+        series = crawler.UolSeries().get(ativos, intraday, periodo, dataini)
         for i in series:
             setattr(self, i[1], i[0])
         setattr(self, 'ativos', ativos)
